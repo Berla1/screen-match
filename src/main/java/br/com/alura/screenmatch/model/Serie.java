d5a80713b921fb38326e5be6e08d105273bbec60
Gustavo Berlanga <74972093+Berla1@users.noreply.github.com>
2025-01-08 13:35:33 -0300

    modelando e usando  classe serie

--- src/main/java/br/com/alura/screenmatch/model/Serie.java
+++ src/main/java/br/com/alura/screenmatch/model/Serie.java
@@ -25,4 +25,78 @@ public class Serie {
         this.sinopse = dadosSerie.sinopse();
 
     }
+
+    @Override
+    public String toString() {
+        return  "genero=" + genero +
+                ", titulo='" + titulo + '\'' +
+                ", totalTemporadas=" + totalTemporadas +
+                ", avaliacao=" + avaliacao +
+                ", atores='" + atores + '\'' +
+                ", poster='" + poster + '\'' +
+                ", sinopse='" + sinopse + '\'';
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public Serie setTitulo(String titulo) {
+        this.titulo = titulo;
+        return this;
+    }
+
+    public String getSinopse() {
+        return sinopse;
+    }
+
+    public Serie setSinopse(String sinopse) {
+        this.sinopse = sinopse;
+        return this;
+    }
+
+    public Integer getTotalTemporadas() {
+        return totalTemporadas;
+    }
+
+    public Serie setTotalTemporadas(Integer totalTemporadas) {
+        this.totalTemporadas = totalTemporadas;
+        return this;
+    }
+
+    public Double getAvaliacao() {
+        return avaliacao;
+    }
+
+    public Serie setAvaliacao(Double avaliacao) {
+        this.avaliacao = avaliacao;
+        return this;
+    }
+
+    public String getAtores() {
+        return atores;
+    }
+
+    public Serie setAtores(String atores) {
+        this.atores = atores;
+        return this;
+    }
+
+    public Categoria getGenero() {
+        return genero;
+    }
+
+    public Serie setGenero(Categoria genero) {
+        this.genero = genero;
+        return this;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public Serie setPoster(String poster) {
+        this.poster = poster;
+        return this;
+    }
 }
